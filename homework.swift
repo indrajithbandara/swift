10e7503f84551401562ce180d99c3c5cd473fc71
Gabrielle <ethernetcabe@gmail.com>
2018-02-02 20:50:30 +0000

    Update homework.swift

--- homework.swift
+++ homework.swift
@@ -1,11 +1,11 @@
 //On the line below, declare the variable, language, again
-var language = "Swift";
+var language = "Swift"
 //Declare the variable, nobleGoal, again as well
-var nobleGoal = "making something fun and safe";
+var nobleGoal = "making something fun and safe"
 
 // Using string interpolation insert the variables, name and nobleGoal into the 
 // string, end.
-var condition = "Then I knew that, programming in  \(language),"; 
+var condition = "Then I knew that, programming in  \(language),"
 var result = "I could make my dream of  \(nobleGoal) a reality."
 
 // Concatenate the variables, condition and result, to create the end of your 
