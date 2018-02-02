40ed2d4b5ad9d40df61c60218ea1830a2559dd1a
Gabrielle <ethernetcabe@gmail.com>
2018-02-02 20:46:33 +0000

    Create homework.swift

---
+++ homework.swift
@@ -0,0 +1,16 @@
+//On the line below, declare the variable, language, again
+var language = "Swift";
+//Declare the variable, nobleGoal, again as well
+var nobleGoal = "making something fun and safe";
+
+// Using string interpolation insert the variables, name and nobleGoal into the 
+// string, end.
+var condition = "Then I knew that, programming in  \(language),"; 
+var result = "I could make my dream of  \(nobleGoal) a reality."
+
+// Concatenate the variables, condition and result, to create the end of your 
+// story
+var end =  condition + result
+
+// Now, on the line below print out the end to your story
+print(condition + result)
